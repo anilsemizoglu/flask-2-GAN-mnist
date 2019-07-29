097b7a3904a654cb0699d40a5622cb3d4655e89d
user <user@users-MacBook-Pro.local>
2019-07-28 23:59:27 -0700

    responsive plotting

--- flask_app.py
+++ flask_app.py
@@ -5,7 +5,7 @@ import os
 os.environ['KMP_DUPLICATE_LIB_OK']='True'
 # above lines are fixes to my weird python environment errors
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 from generate_number import generate_num, InputForm
 
 app = Flask(__name__)
@@ -14,21 +14,25 @@ app = Flask(__name__)
 # def hello_world2():
 #
 #     return render_template('graphs.html')
+@app.route('/')
+def index():
+    return render_template('graphs.html')
 
-@app.route('/',methods=['GET','POST'])
+
+@app.route('/generate_random_number',methods=['GET','POST'])
 def generate_random_number():
-    digit= 1
+    digit= 2
     confidence=.5
     graph1_url=-1
     prob=-1
     i=-1
-    form = InputForm(request.form)
-    if request.method == 'POST' and form.validate():
-        digit = form.digit.data
-        confidence = form.confidence.data
-        graph1_url,prob, i = generate_num(form.digit.data,form.confidence.data)
-    return render_template('graphs.html',graph1=graph1_url,prob=prob,i=i,digit=int(digit),form=form)
+    digit = request.form.get('digit', 0, type=float)
+    confidence = request.form.get('confidence', 0, type=float)
+    graph1_url, prob, i = generate_num(digit,confidence)
+    # print(graph1_url)
+    # return render_template('graphs.html',graph1=graph1_url,prob=prob,i=i,digit=int(digit))
+    return jsonify(result=graph1_url,prob=str(prob))
 
 if __name__ == '__main__':
     app.debug = True
-    app.run(port=5049)
+    app.run(port=5048)
